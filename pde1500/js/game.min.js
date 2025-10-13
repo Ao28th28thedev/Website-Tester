3f469c6f524fcf99944536cbfb879c586424b7cf
Ao28th28thedev <109283781+Ao28th28thedev@users.noreply.github.com>
2025-10-13 17:05:09 -0400

    Add files via upload

--- pde1500/js/game.min.js
+++ pde1500/js/game.min.js
@@ -49558,8 +49558,8 @@ connect: function(e, t) { // 'e' is the world object passed from UI, 't' is like
 }, {
     opponent: {
         data: '{"level":100}',
-        appearance: '{"name":"Flamewielder Trinity", "gender":"female", "hairStyle":5,"hairColor":4,"skinColor":1,"eyeColor":4}',
-        equipment: '{"hat":23, "outfit":53, "weapon":78, "boots":18}'
+        appearance: '{"name":"Surveyor Melody", "gender":"female", "hairStyle":4,"hairColor":9,"skinColor":1,"eyeColor":4}',
+        equipment: '{"hat":23, "outfit":52, "weapon":77, "boots":18}'
     },
     title: "Ao28th28's another alt account from Daboss7173's PDE",
     description: "She is #1 on the arena leaderboards in Daboss7173's PDE.",
@@ -56418,7 +56418,7 @@ var Screen = function() {
 				this.game.prodigy.open.okaymessage("The load character button doesn't work on iPads. We suggest you use another device if you are an iPad user.", null, "star", "Warning!");
 			this.game.prodigy.debug.easyMode(1, 1), this.background.add(this.game.prodigy.create.sprite(0, 0, "login", "bg")), this.loginBox = this.game.prodigy.create.element(this.background), this.usernameField = Prodigy.Control.InputField.createInputField(this.game, this.loginBox, "username", "", 90, 230, 300, 40), this.usernameField.hide(0), this.usernameField.setLabel(this.loginBox, "Prodigy version 1.50.0");
 			var e = Util.getCookie("prodigyUsername");
-			Util.isDefined(e) && this.usernameField.setValue(e), this.passwordField = Prodigy.Control.InputField.createInputField(this.game, this.loginBox, "password", "", 90, 310, 300, 40, "password"), this.passwordField.hide(0), this.passwordField.setLabel(this.loginBox, "Definitive Edition version 24"), this.loadCharacterButton = this.game.prodigy.create.button(this.loginBox, 100, 380, "login", "loadcharacter", this.openFileForCharacter.bind(this)), this.offlineModeButton = this.game.prodigy.create.button(this.loginBox, 100, 470, "login", "google-signin-btn", this.onGoogleLoginButtonClick.bind(this)), this.progressBox = this.game.prodigy.create.element(this.background, 100, 250), this.error = this.game.prodigy.create.font(this.progressBox, 0, 0, "", {
+			Util.isDefined(e) && this.usernameField.setValue(e), this.passwordField = Prodigy.Control.InputField.createInputField(this.game, this.loginBox, "password", "", 90, 310, 300, 40, "password"), this.passwordField.hide(0), this.passwordField.setLabel(this.loginBox, "Definitive Edition version 25"), this.loadCharacterButton = this.game.prodigy.create.button(this.loginBox, 100, 380, "login", "loadcharacter", this.openFileForCharacter.bind(this)), this.offlineModeButton = this.game.prodigy.create.button(this.loginBox, 100, 470, "login", "google-signin-btn", this.onGoogleLoginButtonClick.bind(this)), this.progressBox = this.game.prodigy.create.element(this.background, 100, 250), this.error = this.game.prodigy.create.font(this.progressBox, 0, 0, "", {
 				width: 300,
 				align: "center"
 			}), this.closeButton = this.game.prodigy.create.textButton(this.progressBox, 0, 100, {
